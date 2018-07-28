03e77986cc1403f52014beba66dcef851156a33d
Megha K.C <meghakc10@gmail.com>
2018-07-28 13:10:50 +0530

    Login page

--- app/src/main/java/universe/sk/syndrive_hackathon/MainActivity.java
+++ app/src/main/java/universe/sk/syndrive_hackathon/MainActivity.java
@@ -2,12 +2,25 @@ package universe.sk.syndrive_hackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etEmail,etPassword;
+    private TextView tvForgotPassword;
+    private Button btnLogin;
+    //private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        tvForgotPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
     }
 }
