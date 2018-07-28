4b76e55ef8e73d52e8e6d59835d90637a3c50668
AshmiFathima <ashmi3395@gmail.com>
2018-07-28 17:04:56 +0530

    changes

--- app/src/main/java/universe/sk/syndrive_hackathon/AboutActivity.java
+++ app/src/main/java/universe/sk/syndrive_hackathon/AboutActivity.java
@@ -1,45 +1,29 @@
 package universe.sk.syndrive_hackathon;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
 
 public class AboutActivity extends AppCompatActivity {
 
-    private EditText etName, etEmailsign, etPassign, etConfirmPassign, etBloodgroup, etDate, etEmName1, etEmName2, etEmName3, etEmNum1, etEmNum2, etEmNum3;
-    private Button btn_register;
-    private TextView tvExist;
-    private FirebaseAuth firebaseAuth;
-    String name, email, password, bloodgrp, date;
-    String emname1, emname2, emname3;
-    String emnum1, emnum2, emnum3;
+
+    TextView tvAbout,tvAbout2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-    }
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(" About");
+        actionBar.setIcon(R.drawable.about);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
-    private void setUIviews() {
-        etName = findViewById(R.id.etName);
-        etEmName1 = findViewById(R.id.etEmName1);
-        etEmName2 = findViewById(R.id.etEmName2);
-        etEmName3 = findViewById(R.id.etEmName3);
-        etBloodgroup = findViewById(R.id.etBloodgroup);
-        etEmNum1 = findViewById(R.id.etEmNum1);
-        etEmNum2 = findViewById(R.id.etEmNum2);
-        etEmNum3 = findViewById(R.id.etEmNum3);
-        etEmailsign = findViewById(R.id.etEmailsign);
-        etPassign = findViewById(R.id.etPassign);
-        etConfirmPassign = findViewById(R.id.etConfirmPassign);
-        etDate = findViewById(R.id.etDate);
-        btn_register = findViewById(R.id.btn_register);
-        tvExist = findViewById(R.id.tvExist);
+        tvAbout = findViewById(R.id.tvAbout);
+        tvAbout2 = findViewById(R.id.tvAbout2);
 
     }
 }
