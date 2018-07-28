517c2be280f205580c4bed1b1cde556acba2619a
AshmiFathima <ashmi3395@gmail.com>
2018-07-28 16:41:10 +0530

    change

---
+++ app/src/main/java/universe/sk/syndrive_hackathon/AboutActivity.java
@@ -0,0 +1,30 @@
+package universe.sk.syndrive_hackathon;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AboutActivity extends AppCompatActivity {
+
+
+    TextView tvAbout,tvAbout2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(" About");
+        actionBar.setIcon(R.drawable.about);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        tvAbout = findViewById(R.id.tvAbout);
+        tvAbout2 = findViewById(R.id.tvAbout2);
+
+    }
+}
+
