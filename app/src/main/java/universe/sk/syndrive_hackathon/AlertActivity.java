c0e24b04ba0ad5295ea97e44f69cca54f374941d
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-07-28 17:04:24 +0530

    Alert Activity

--- app/src/main/java/universe/sk/syndrive_hackathon/AlertActivity.java
+++ app/src/main/java/universe/sk/syndrive_hackathon/AlertActivity.java
@@ -1,7 +1,18 @@
 package universe.sk.syndrive_hackathon;
 
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class AlertActivity extends AppCompatActivity {
 
+    TextView tvTime;
+    FloatingActionButton fabSend, fabDismiss;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
 }
